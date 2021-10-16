beb380cd32e076158d11987802821f7e82217a98
mag1cianag <mag1cian@aliyun.com>
2021-10-16 19:14:52 +0800

    lab3 done

--- lab3/Flik/FlikTest.java
+++ lab3/Flik/FlikTest.java
@@ -1,9 +1,11 @@
 import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class FlikTest {
     @Test
     public void testIsSameNumber() {
-        assertTrue("128 is not same",Flik.isSameNumber(128,128));
-        assertTrue("200 is not same",Flik.isSameNumber(200,200));
+        assertTrue("128 is not same", Flik.isSameNumber(128, 128));
+        assertTrue("200 is not same", Flik.isSameNumber(200, 200));
     }
 }
--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,13 +1,13 @@
 public class HorribleSteve {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         int i = 0;
         int j = 0;
         while (j < 500) {
-           if(!Flik.isSameNumber(i,j)) {
-               break;
-           }
-           i+=1;
-           j+=1;
+            if (!Flik.isSameNumber(i, j)) {
+                break;
+            }
+            i += 1;
+            j += 1;
         }
         System.out.println("i is " + i);
     }
